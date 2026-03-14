7fa66cf2722eabb647e75ed4740c5b89a3720ea8
Ninnin <152572819+Geeyannn@users.noreply.github.com>
2026-03-14 23:56:03 +0800

    Implement dynamic array template structure

--- include/array.h
+++ include/array.h
@@ -1,1 +1,42 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+
+template <typename T>
+struct array
+{
+	array(size_t n)
+	{
+		a = new T[n];
+		length = n;
+	}
+
+	~array()
+	{
+		delete[] a;
+	}
+
+	T& operator[](size_t i)
+	{
+		assert(i < length);
+		return a[i];
+	}
+
+	array<T>& operator=(array<T>& other)
+	{
+		if (a != nullptr)
+		{
+			delete[] a;
+		}
+		a = other.a;
+		other.a = nullptr;
+		length = other.length;
+		other.length = 0;
+		return *this;
+	}
+
+	size_t length;
+	T* a;
+};
 
